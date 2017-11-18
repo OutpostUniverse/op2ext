a7300d089b16a053fb7e16cdf5bf52939cffda83
Brett208 <15183337+Brett208@users.noreply.github.com>
2017-11-18 12:25:56 -1000

    Rewrote ModMgr's function GetCurrentModDir() to use modern C++ features
     * Improved error messaging to user when improper command line arguments or switches provided.
     * Started prepping ModMgr.h/.cpp for refactor into a class. (Would like to encapsulate private functions).
    
    I'm not entirely happy with the new code. It is lengthy and not easy to follow when reading through. I think it is an improvement over the last iteration because it removes the deeply nested token search statement. While there are more lines of code overall, they are separated into smaller functions. GetCurrentModDir itself is still too lengthy.
    
    This function could probably be improved by using exceptions, but we are not using exceptions anywhere else. So I limited exception use to just where required when accessing functions in the std library that leverage exceptions.
    
    I'd like to rename ModMgr.h/.cpp to ModManager.h/.cpp. I'm unsure if this would affect c linkage to the function? I don't believe it would though.
    
    Function tested reasonably thoroughly through different failure conditions (bad path, no path, wrong spelled switch, etc) without finding bugs.

--- ModMgr.cpp
+++ ModMgr.cpp
@@ -1,71 +1,102 @@
-// ModMgr.cpp
 #include "ModMgr.h"
-#include "op2ext.h"
 
-char newOut2resPath[MAX_PATH + 1];
-char newOp2shresPath[MAX_PATH + 1];
-char cmdLine[MAX_PATH + 1];
+#include "op2ext.h"
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <filesystem>
+
+namespace fs = std::experimental::filesystem;
+
 HMODULE modDllHandle = nullptr;
 
-
 EXPORT char* GetCurrentModDir()
-{
+{ 
 	DBG("In ModMgr.cpp: GetCurrentModDir()\n");
 
-	// Get the current command line
-	strncpy_s(cmdLine, GetCommandLineA(), MAX_PATH);
+	std::vector<std::string> arguments;
+	parseCommandLine(arguments);
 
-	// Scan the string
-	char* strtokState;
-	char* part = strtok_s(cmdLine, " ", &strtokState);
-
-	if (part == nullptr) {
+	if (arguments.size() == 0) {
 		return nullptr;
 	}
 
-	// While there are more parts to the string left...
-	while (part)
-	{
-		// see if it starts with the option character (/ or -)
-		if (part[0] == '/' || part[0] == '-')
-		{
-			// is the option data "loadmod"?
-			if (_strcmpi(&part[1], "loadmod") == 0)
-			{
-				// get the next part
-				char* modName = strtok_s(nullptr, ": ", &strtokState);
-				// Check for invalid chars and other things
-				if (modName == nullptr)
-				{
-					PostErrorMessage("ModMgr.cpp", __LINE__, "Argument missing");
-					return nullptr;
-				}
-				if (strpbrk(modName, "\\/:*?\"<>|") != nullptr)
-				{
-					PostErrorMessage("ModMgr.cpp", __LINE__, "Invalid directory name");
-					return nullptr;
-				}
-
-				// see if it's a real directory
-				char modDir[MAX_PATH + 1];
-				GetGameDir(modDir);
-				strcat_s(modDir, modName);
-
-				if (GetFileAttributesA(modDir) == -1)
-				{
-					PostErrorMessage("ModMgr.cpp", __LINE__, "Mod directory does not exist");
-					return nullptr;
-				}
-
-				// Looks good.
-				return _strdup(modDir);
-			}
-		}
-		// get next argument
-		part = strtok_s(nullptr, " ", &strtokState);
+	std::string switchName = arguments[0];
+	arguments.erase(arguments.begin());
+	
+	if (!ParseArgumentName(switchName)) {
+		return nullptr;
 	}
 
-	return nullptr;
+	if (switchName != "loadmod") {
+		PostErrorMessage("ModMgr.cpp", __LINE__, "Provided switch is not supported: " + switchName);
+		return nullptr;
+	}
+
+	std::string modDirectory = ParseLoadModCommand(arguments);
+
+	if (modDirectory.empty()) {
+		return nullptr;
+	}
+
+	char* cStr = new char[modDirectory.length() + 1];
+	return strcpy(cStr, modDirectory.c_str());
+}
+
+void parseCommandLine(std::vector<std::string>& arguments)
+{
+	//GetCommandLineA returns a LPSTR (long pointer to a string). For use in ANSI (A postfix).
+	std::string commandLine = GetCommandLineA();
+
+	std::istringstream iss(commandLine);
+	std::copy(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(), std::back_inserter(arguments));
+
+	arguments.erase(arguments.begin()); //Remove Application Name from arguments.
+}
+
+bool ParseArgumentName(std::string& argument)
+{
+	if (argument[0] != '/' && argument[0] != '-') {
+		PostErrorMessage("ModMgr.cpp", __LINE__, "A switch was expected but not found. Prefix switch name with '/' or '-'.");
+		argument.clear();
+		return false;
+	}
+
+	argument.erase(argument.begin(), argument.begin() + 1);
+	std::transform(argument.begin(), argument.end(), argument.begin(), ::tolower);
+
+	return true;
+}
+
+std::string ParseLoadModCommand(std::vector<std::string> arguments)
+{
+	if (arguments.empty()) {
+		PostErrorMessage("ModMgr.cpp", __LINE__, "No relative directory argument provided for the switch loadmod");
+		return std::string();
+	}
+
+	fs::path p(GetGameDirectory());
+
+	std::string modRelativeDirectory;
+	for (auto & argument : arguments) {
+		modRelativeDirectory += argument;
+	}
+
+	try
+	{
+		std::string modDirectory = p.append(modRelativeDirectory).string();
+
+		if (GetFileAttributesA(modDirectory.c_str()) == -1) {
+			PostErrorMessage("ModMgr.cpp", __LINE__, "Mod directory does not exist: " + modDirectory);
+			return std::string();
+		}
+
+		return modDirectory;
+	}
+	catch (std::exception e)
+	{
+		PostErrorMessage("ModMgr.cpp", __LINE__, "Unable to parse mod directory");
+	}
 }
 
 void ApplyMod(char *modDir)
--- ModMgr.h
+++ ModMgr.h
@@ -1,9 +1,17 @@
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
 #include "op2ext.h"
 
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <string>
+#include <vector>
 
 EXPORT char* GetCurrentModDir();
 void ApplyMod(char *modDir);
 void UnApplyMod();
 void ModStartup();
+
+// private:
+void parseCommandLine(std::vector<std::string>& arguments);
+bool ParseArgumentName(std::string& argument);
+std::string ParseLoadModCommand(std::vector<std::string> arguments);
