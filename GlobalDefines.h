f52c27112e59a50d5d8dec6563fdeab83b17dc99
Brett208 <15183337+Brett208@users.noreply.github.com>
2018-12-21 15:34:42 -0500

    Log messages to the log file that are processed by PostErrorMessage

--- GlobalDefines.h
+++ GlobalDefines.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+// Outputs a debug string. Output is not logged by the logger.
 void OutputDebug(std::string message);
 
 // Defines how leading and trailing characters of a string are trimmed. 
@@ -14,6 +15,7 @@ enum class TrimOption
 	None
 };
 
+// Logs an error message with the logger and then posts it to user in a modal dialog box.
 void PostErrorMessage(std::string sourceFilename, long lineInSourceCode, std::string errorMessage);
 std::vector<std::string> SplitString(std::string stringToSplit, char delimiter, TrimOption trimOption);
 std::string TrimString(const std::string& stringToTrim, TrimOption trimOption, const std::string& whitespace = " \t");
