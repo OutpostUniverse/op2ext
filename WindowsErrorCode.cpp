f680a80baa5277855dda2c4e4db8d1ae2936f36e
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-03-09 22:05:12 -0500

    Allow pulling the last error code from Windows API calls
    Translates error code into a std::string before returning.

---
+++ WindowsErrorCode.cpp
@@ -0,0 +1,36 @@
+#include "WindowsErrorCode.h"
+#include "StringConversion.h"
+#include <strsafe.h> //Header requires WindowsXP SP2 or greater
+
+std::string GetLastErrorStdString(LPTSTR lpszFunction)
+{
+	// Adapted from https://docs.microsoft.com/en-us/windows/desktop/Debug/retrieving-the-last-error-code
+
+	LPVOID lpMsgBuf;
+	DWORD dw = GetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	LPVOID lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+	
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%s failed with error %d: %s"),
+		lpszFunction, dw, lpMsgBuf);
+
+	std::string errorMessage = ConvertLpctstrToString(static_cast<LPCTSTR>(lpDisplayBuf));
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+
+	return errorMessage;
+}
---
+++ WindowsErrorCode.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <windows.h>
+#include <string>
+
+// Retrieve the system error message for the last-error code
+std::string GetLastErrorStdString(LPTSTR lpszFunction);
