def216c1bfdc99a4643b221713ddd92d20c12261
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-03-23 18:27:20 +0700

    Use unsigned literals to avoid warning

--- test/op2ext.test.cpp
+++ test/op2ext.test.cpp
@@ -8,12 +8,12 @@ using bufferType = std::array<char, 8>;
 
 
 TEST(CopyStdStringIntoCharBuffer, NullDestination) {
-	EXPECT_EQ(5, CopyStdStringIntoCharBuffer(std::string("Test"), nullptr, 0));
+	EXPECT_EQ(5u, CopyStdStringIntoCharBuffer(std::string("Test"), nullptr, 0));
 }
 
 TEST(CopyStdStringIntoCharBuffer, EmptyDestination) {
 	bufferType buffer{};
-	EXPECT_EQ(5, CopyStdStringIntoCharBuffer(std::string("Test"), buffer.data(), 0));
+	EXPECT_EQ(5u, CopyStdStringIntoCharBuffer(std::string("Test"), buffer.data(), 0));
 	EXPECT_EQ(bufferType{}, buffer);
 }
 
@@ -21,18 +21,18 @@ TEST(CopyStdStringIntoCharBuffer, TooShortSizeDestination) {
 	bufferType buffer{};
 	// Copy with no room for full string + null terminator
 	// Should copy most of the string, and null terminate
-	EXPECT_EQ(5, CopyStdStringIntoCharBuffer(std::string("Test"), buffer.data(), 4));
+	EXPECT_EQ(5u, CopyStdStringIntoCharBuffer(std::string("Test"), buffer.data(), 4));
 	EXPECT_EQ("Tes", std::string(buffer.data()));
 }
 
 TEST(CopyStdStringIntoCharBuffer, MinimumSizeDestination) {
 	bufferType buffer{};
-	EXPECT_EQ(0, CopyStdStringIntoCharBuffer(std::string("Test"), buffer.data(), 5));
+	EXPECT_EQ(0u, CopyStdStringIntoCharBuffer(std::string("Test"), buffer.data(), 5));
 	EXPECT_EQ("Test", std::string(buffer.data()));
 }
 
 TEST(CopyStdStringIntoCharBuffer, NormalDestination) {
 	bufferType buffer{};
-	EXPECT_EQ(0, CopyStdStringIntoCharBuffer(std::string("Test"), buffer.data(), sizeof(buffer)));
+	EXPECT_EQ(0u, CopyStdStringIntoCharBuffer(std::string("Test"), buffer.data(), sizeof(buffer)));
 	EXPECT_EQ("Test", std::string(buffer.data()));
 }
