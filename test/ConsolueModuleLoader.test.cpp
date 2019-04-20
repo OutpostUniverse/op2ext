7d9e391a94a163d9f40ad8dcce4d2df43ad26810
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-04-20 10:29:42 -0400

    Reorder EXPECT_EQ parameters

--- test/ConsolueModuleLoader.test.cpp
+++ test/ConsolueModuleLoader.test.cpp
@@ -8,7 +8,7 @@ TEST(ConsoleModuleLoader, NoModuleLoaded)
 	ConsoleModuleLoader consoleModLoader;
 
 	// Returns empty string if no module available
-	EXPECT_EQ(consoleModLoader.GetModuleName(), "");
+	EXPECT_EQ("", consoleModLoader.GetModuleName());
 
 	EXPECT_TRUE(consoleModLoader.IsModuleLoaded(""));
 	EXPECT_FALSE(consoleModLoader.IsModuleLoaded("TEST"));
@@ -19,7 +19,7 @@ TEST(ConsoleModuleLoader, GetModuleDirectory)
 	const std::string testModule("TestModule");
 	ConsoleModuleLoader consoleModLoader(testModule);
 
-	EXPECT_NE(consoleModLoader.GetModuleDirectory(), "");
+	EXPECT_NE("", consoleModLoader.GetModuleDirectory());
 	EXPECT_EQ(testModule, consoleModLoader.GetModuleDirectory());
 }
 
@@ -28,6 +28,6 @@ TEST(ConsoleModuleLoader, GetModuleName)
 	const std::string testModule("testmodule");
 	ConsoleModuleLoader consoleModLoader(testModule);
 
-	EXPECT_NE(consoleModLoader.GetModuleName(), "");
+	EXPECT_NE("", consoleModLoader.GetModuleName());
 	EXPECT_EQ(testModule, consoleModLoader.GetModuleName());
 }
