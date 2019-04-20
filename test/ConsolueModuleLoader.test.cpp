03a87536072001dca6f35d26a805bccc351ee30c
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-04-19 20:35:17 -0400

    Add a console module loader test file

---
+++ test/ConsolueModuleLoader.test.cpp
@@ -0,0 +1,10 @@
+#include "ConsoleModuleLoader.h"
+#include <gtest/gtest.h>
+
+TEST(ConsoleModuleLoader, GetModuleDirectory)
+{
+	ConsoleModuleLoader consoleModLoader;
+
+	// Returns empty string if no module available
+	EXPECT_EQ(consoleModLoader.GetModuleDirectory(), "");
+}
