f97c14cf44412dc8b2d9a33d25e8a34a92809f41
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-04-19 21:25:56 -0400

    Combine ConsoleModuleLoader tests into single NoModuleLoaded test

--- test/ConsolueModuleLoader.test.cpp
+++ test/ConsolueModuleLoader.test.cpp
@@ -1,25 +1,13 @@
 #include "ConsoleModuleLoader.h"
 #include <gtest/gtest.h>
 
-TEST(ConsoleModuleLoader, GetModuleDirectory)
-{
-	ConsoleModuleLoader consoleModLoader;
 
-	// Returns empty string if no module available
-	EXPECT_EQ(consoleModLoader.GetModuleDirectory(), "");
-}
-
-TEST(ConsoleModuleLoader, GetModuleName)
+TEST(ConsoleModuleLoader, NoModuleLoaded)
 {
 	ConsoleModuleLoader consoleModLoader;
 
 	// Returns empty string if no module available
 	EXPECT_EQ(consoleModLoader.GetModuleName(), "");
-}
-
-TEST(ConsoleModuleLoader, IsModuleLoaded)
-{
-	ConsoleModuleLoader consoleModLoader;
 
 	EXPECT_TRUE(consoleModLoader.IsModuleLoaded(""));
 	EXPECT_FALSE(consoleModLoader.IsModuleLoaded("TEST"));
