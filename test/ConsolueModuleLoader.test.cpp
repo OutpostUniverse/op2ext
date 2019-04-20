51e9c15544235c70b1a18bb50c54a1b743b041f8
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-04-19 21:27:53 -0400

    Use new ConsolueModuleLoader test constructor in unit tests

--- test/ConsolueModuleLoader.test.cpp
+++ test/ConsolueModuleLoader.test.cpp
@@ -1,5 +1,6 @@
 #include "ConsoleModuleLoader.h"
 #include <gtest/gtest.h>
+#include <string>
 
 
 TEST(ConsoleModuleLoader, NoModuleLoaded)
@@ -11,4 +12,22 @@ TEST(ConsoleModuleLoader, NoModuleLoaded)
 
 	EXPECT_TRUE(consoleModLoader.IsModuleLoaded(""));
 	EXPECT_FALSE(consoleModLoader.IsModuleLoaded("TEST"));
+}
+
+TEST(ConsoleModuleLoader, GetModuleDirectory)
+{
+	const std::string testModule("TestModule");
+	ConsoleModuleLoader consoleModLoader(testModule);
+
+	EXPECT_NE(consoleModLoader.GetModuleDirectory(), "");
+	EXPECT_EQ(testModule, consoleModLoader.GetModuleDirectory());
+}
+
+TEST(ConsoleModuleLoader, GetModuleName)
+{
+	const std::string testModule("testmodule");
+	ConsoleModuleLoader consoleModLoader(testModule);
+
+	EXPECT_NE(consoleModLoader.GetModuleName(), "");
+	EXPECT_EQ(testModule, consoleModLoader.GetModuleName());
 }
