29a0760c8d26505f102f7bf173b32191b49de42c
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-04-26 18:00:24 +0700

    Remove accidentally committed test line
    Previously I was checking the returned path against the current working
    directory. Often this matches the exe path, but not when starting the
    exe with a remote path, such as is done in the testing code. The other
    reason was to get a dump of the current exe path, which is better
    handled with the `<<` operator on the `EXPECT_*` tests.

--- test/FileSystemHelper.test.cpp
+++ test/FileSystemHelper.test.cpp
@@ -12,7 +12,6 @@ namespace fs = std::experimental::filesystem;
 
 TEST(FileSystemHelper, GetGameDirectory) {
 	auto gameDirectory = fs::path(GetGameDirectory());
-	auto currentDirectory = fs::current_path();
 	EXPECT_TRUE(gameDirectory.is_absolute()) << gameDirectory;
 	EXPECT_TRUE(fs::exists(gameDirectory)) << gameDirectory;
 	EXPECT_TRUE(fs::is_directory(gameDirectory)) << gameDirectory;
