8740156dde8745d5983f9aba8923ad1a2c1040f0
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-05-03 21:22:35 -0400

    Remove test of deprecated function that caused a debug assertion failure

--- TestModule/TestFunctions.h
+++ TestModule/TestFunctions.h
@@ -6,6 +6,5 @@ void TestLoggingMessage();
 void TestGetGameDir_s();
 void TestGetGameDir(); //Deprecated
 void TestGetConsoleModDir_s();
-void TestGetConsoleModDir(); //Deprecated
 void TestLoadingVolumes(); //Test loading a non-existent volume & loading too many volumes
 void TestIniSectionName(std::string sectionName);
