6741f073351db2ebbfac389354d3243f153e554c
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-05-11 21:21:03 +0700

    Add hook for `GetFilePath` to `ConsoleModuleLoader`
    This provides an alternative way to hook the resource search code in a
    way that can scale to multiple paths. This could allow the
    `ConsoleModuleLoader` to support loading multiple modules.

--- srcStatic/ConsoleModuleLoader.cpp
+++ srcStatic/ConsoleModuleLoader.cpp
@@ -123,6 +123,56 @@ void ConsoleModuleLoader::SetArtPath()
 	}
 }
 
+void ConsoleModuleLoader::HookFileSearchPath()
+{
+	const std::vector<std::size_t> callsToGetFilePath{
+		0x00402E4B,
+		0x004038A9,
+		0x0045003C,
+		0x0045035D,
+		0x0046078C,
+		0x00460B13,
+		0x00471089,
+		0x0047118B,
+		0x0047121E,
+		0x004712B7,
+		0x00485C69,
+		0x004882CB,
+		0x00488967,
+		0x00489433,
+		0x004977E4,
+	};
+	// Convert a pointer to member function to a regular `void*` value
+	auto getFilePath = &ConsoleModuleLoader::ResManager::GetFilePath;
+	const auto getFilePathAddr = reinterpret_cast<void*&>(getFilePath);  // MSVC specific cast
+
+	for (const auto callAddr : callsToGetFilePath) {
+		Op2RelinkCall(callAddr, getFilePathAddr);
+	}
+}
+
+bool ConsoleModuleLoader::CallOriginalGetFilePath(const char* resourceName, /* [out] */ char* filePath)
+{
+	// Use Outpost2.exe's built in ResManager object, and its associated member function
+	ConsoleModuleLoader::ResManager& resManager = *reinterpret_cast<ResManager*>(0x56C028);
+	using GetFilePathType = decltype(&ConsoleModuleLoader::ResManager::GetFilePath);
+	void* originalGetFilePathAddr = reinterpret_cast<void*>(0x00471590);
+	auto originalGetFilePath = reinterpret_cast<GetFilePathType&>(originalGetFilePathAddr); // MSVC specific cast
+	return (resManager.*originalGetFilePath)(resourceName, filePath);
+}
+
+bool ConsoleModuleLoader::ResManager::GetFilePath(const char* resourceName, /* [out] */ char* filePath) const
+{
+	// Search for resource in module folder
+	const auto path = moduleDirectory + resourceName;
+	if (INVALID_FILE_ATTRIBUTES != GetFileAttributesA(path.c_str())) {
+		return 0 == CopyStdStringIntoCharBuffer(path, filePath, MAX_PATH);
+	}
+
+	// Fallback to searching with the original built in method
+	return CallOriginalGetFilePath(resourceName, filePath);
+}
+
 void ConsoleModuleLoader::UnloadModule()
 {
 	if (modDllHandle)
