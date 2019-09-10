95ce1b09d6fe17a04dc488ede69054bd29eb6a72
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-09-10 14:51:27 -0400

    Write an abstract GameModule class

---
+++ srcStatic/GameModules/GameModule.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+// Abstract class allowing memory modification of Outpost 2 to alter gameplay or fix bugs
+class GameModule
+{
+public:
+	GameModule(const std::string& name) : name(name) { };
+	virtual ~GameModule() = default;
+	inline std::string Name() const { return name; };
+
+	virtual void Initialize() = 0; //mod_init(), InitMod
+	// Only available for Console modules (other modules would just call an empty function)
+	//virtual void Run() = 0; //mod_run()
+	virtual bool Destroy() = 0; //mod_destroy(), DestroyMod
+
+private:
+	const std::string name;
+};
