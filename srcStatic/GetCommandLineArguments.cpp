c1487912a760e2ebd96da54179ba22f3af6ff348
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-09-18 17:05:01 -0600

    Split `GetCommandLineArguments` to new file
    This method is a bit unlike the other parsing methods. For one, it's
    quite general, and usable in many contexts. For another, it's the only
    method that actually touches the Windows API. By splitting it into a new
    file, we can limit the code that needs to include `<windows.h>`. By
    creating a code firewall here, we can limit recompilation times should
    the other code be updated.

---
+++ srcStatic/GetCommandLineArguments.cpp
@@ -0,0 +1,37 @@
+#include "GetCommandLineArguments.h"
+#include "StringConversion.h"
+#include "GlobalDefines.h"
+#include <windows.h> // Cannot use WIN32_LEAN_AND_MEAN (it does not contain CommandLineToArgvW)
+
+
+std::vector<std::string> GetCommandLineArguments()
+{
+	std::vector<std::string> arguments;
+	int argumentCount;
+	LPWSTR* commandLineArgs = CommandLineToArgvW(GetCommandLineW(), &argumentCount);
+
+	if (commandLineArgs == nullptr) {
+		PostErrorMessage(__FILE__, __LINE__, "Unable to retrieve command line arguments attached to Outpost2.exe.");
+	}
+	else {
+		try {
+			// Ignore the first argument, which is the path of the executable.
+			for (int i = 1; i < argumentCount; ++i) {
+				std::string argument;
+				if (!ConvertLPWToString(argument, commandLineArgs[i])) {
+					PostErrorMessage(__FILE__, __LINE__, "Unable to cast the " + std::to_string(i) +
+						" command line argument from LPWSTR to char*. Further parsing of command line arguments aborted.");
+					break;
+				}
+				arguments.push_back(argument);
+			}
+		}
+		// Catch STL produced exceptions
+		catch (const std::exception& e) {
+			PostErrorMessage(__FILE__, __LINE__, "Error occurred attempting to parse command line arguments. Further parsing of command line arguments aborted. Internal Error: " + std::string(e.what()));
+		}
+	}
+
+	LocalFree(commandLineArgs);
+	return arguments;
+}
