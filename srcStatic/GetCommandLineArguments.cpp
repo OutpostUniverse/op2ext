5141cc2f99be98ffd965b735b9aff44a8559459f
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-09-18 21:49:23 -0600

    Remove try/catch from `GetCommandLineArguments`
    Now that the caller is expected to catch exceptions, and possibly report
    them, there is considerably less value in catching exceptions internally
    and trying to report them.

--- srcStatic/GetCommandLineArguments.cpp
+++ srcStatic/GetCommandLineArguments.cpp
@@ -15,20 +15,14 @@ std::vector<std::string> GetCommandLineArguments()
 		throw std::runtime_error("Unable to retrieve command line arguments attached to Outpost2.exe.");
 	}
 	else {
-		try {
-			// Ignore the first argument, which is the path of the executable.
-			for (int i = 1; i < argumentCount; ++i) {
-				std::string argument;
-				if (!ConvertLPWToString(argument, commandLineArgs[i])) {
-					throw std::runtime_error("Unable to convert command line argument from LPWSTR to std::string. Argument index: " + std::to_string(i));
-					break;
-				}
-				arguments.push_back(argument);
+		// Ignore the first argument, which is the path of the executable.
+		for (int i = 1; i < argumentCount; ++i) {
+			std::string argument;
+			if (!ConvertLPWToString(argument, commandLineArgs[i])) {
+				throw std::runtime_error("Unable to convert command line argument from LPWSTR to std::string. Argument index: " + std::to_string(i));
+				break;
 			}
-		}
-		// Catch STL produced exceptions
-		catch (const std::exception& e) {
-			throw std::runtime_error("Error parsing command line arguments. Internal Error: " + std::string(e.what()));
+			arguments.push_back(argument);
 		}
 	}
 
