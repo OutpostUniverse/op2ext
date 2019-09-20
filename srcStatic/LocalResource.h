732edb4e043e3f2f19c872407570c20ca51d2b96
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-09-19 18:01:25 -0600

    Add `is_pointer` static check to `LocalResource`
    This will provide better error messages if `LocalResource` is used with
    an inappropriate base type.

--- srcStatic/LocalResource.h
+++ srcStatic/LocalResource.h
@@ -1,9 +1,12 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <type_traits>
+
 
 // RAII management of LocalAlloc/LocalFree resources
 template <typename T>
 class LocalResource {
+	static_assert(std::is_pointer<T>::value, "Type must be a pointer");
 public:
 	LocalResource(T pointer) : pointer(pointer) {
 	}
