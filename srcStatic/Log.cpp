8ba7eda66ed562f4df6195c750e0b0567861877a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-09-20 23:29:13 -0600

    Add helper method to set the active logger

--- srcStatic/Log.cpp
+++ srcStatic/Log.cpp
@@ -2,6 +2,13 @@
 #include "op2ext-Internal.h"
 
 
+// Set logging output destination
+// Caller owns the logger and is responsible for cleanup when logger is no longer required
+// Use `SetLogger(nullptr);` to unset a logger
+void SetLogger(Logger* newLogger) {
+	logger = newLogger;
+}
+
 void Log(const std::string& message, const std::string& moduleName) {
 	// Delegate to internal logger
 	logger->Log(message, moduleName);
--- srcStatic/Log.h
+++ srcStatic/Log.h
@@ -1,4 +1,8 @@
 #include <string>
 
 
+class Logger;
+
+
+void SetLogger(Logger* logger);
 void Log(const std::string& message, const std::string& moduleName = "op2ext.dll");
