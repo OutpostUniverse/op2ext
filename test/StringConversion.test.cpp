4af91a32a36ba149ca6c71cd19255d0ba3ad8e67
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-09-23 09:29:28 -0600

    Update unit tests for 3 new `Trim` functions

--- test/StringConversion.test.cpp
+++ test/StringConversion.test.cpp
@@ -119,3 +119,129 @@ TEST(StringConversion, TrimString)
 	EXPECT_EQ("A\t A", TrimString("A\t A"));
 	EXPECT_EQ("A\t\tA", TrimString("A\t\tA"));
 }
+
+TEST(StringConversion, Trim)
+{
+	// Empty string
+	EXPECT_EQ("", Trim(""));
+
+	// Only whitespace
+	EXPECT_EQ("", Trim(" "));
+	EXPECT_EQ("", Trim("\t"));
+	EXPECT_EQ("", Trim("\t "));
+	EXPECT_EQ("", Trim(" \t"));
+	EXPECT_EQ("", Trim("\t\t"));
+
+	// Only no whitespace
+	EXPECT_EQ("A", Trim("A"));
+	EXPECT_EQ("AA", Trim("AA"));
+	EXPECT_EQ("ABC", Trim("ABC"));
+
+	// Leading whitespace
+	EXPECT_EQ("A", Trim(" A"));
+	EXPECT_EQ("A", Trim("\tA"));
+
+	// Trailing whitespace
+	EXPECT_EQ("A", Trim("A "));
+	EXPECT_EQ("A", Trim("A\t"));
+
+	// Leading and trailing whitespace
+	EXPECT_EQ("A", Trim(" A "));
+	EXPECT_EQ("A", Trim("\tA\t"));
+	EXPECT_EQ("A", Trim("  A  "));
+	EXPECT_EQ("A", Trim(" \tA\t "));
+	EXPECT_EQ("A", Trim("\t A \t"));
+	EXPECT_EQ("A", Trim("\t\tA\t\t"));
+
+	// Embedded whitespace
+	EXPECT_EQ("A A", Trim("A A"));
+	EXPECT_EQ("A\tA", Trim("A\tA"));
+	EXPECT_EQ("A  A", Trim("A  A"));
+	EXPECT_EQ("A \tA", Trim("A \tA"));
+	EXPECT_EQ("A\t A", Trim("A\t A"));
+	EXPECT_EQ("A\t\tA", Trim("A\t\tA"));
+}
+
+TEST(StringConversion, TrimFront)
+{
+	// Empty string
+	EXPECT_EQ("", TrimFront(""));
+
+	// Only whitespace
+	EXPECT_EQ("", TrimFront(" "));
+	EXPECT_EQ("", TrimFront("\t"));
+	EXPECT_EQ("", TrimFront("\t "));
+	EXPECT_EQ("", TrimFront(" \t"));
+	EXPECT_EQ("", TrimFront("\t\t"));
+
+	// Only no whitespace
+	EXPECT_EQ("A", TrimFront("A"));
+	EXPECT_EQ("AA", TrimFront("AA"));
+	EXPECT_EQ("ABC", TrimFront("ABC"));
+
+	// Leading whitespace
+	EXPECT_EQ("A", TrimFront(" A"));
+	EXPECT_EQ("A", TrimFront("\tA"));
+
+	// Trailing whitespace
+	EXPECT_EQ("A ", TrimFront("A "));
+	EXPECT_EQ("A\t", TrimFront("A\t"));
+
+	// Leading and trailing whitespace
+	EXPECT_EQ("A ", TrimFront(" A "));
+	EXPECT_EQ("A\t", TrimFront("\tA\t"));
+	EXPECT_EQ("A  ", TrimFront("  A  "));
+	EXPECT_EQ("A\t ", TrimFront(" \tA\t "));
+	EXPECT_EQ("A \t", TrimFront("\t A \t"));
+	EXPECT_EQ("A\t\t", TrimFront("\t\tA\t\t"));
+
+	// Embedded whitespace
+	EXPECT_EQ("A A", TrimFront("A A"));
+	EXPECT_EQ("A\tA", TrimFront("A\tA"));
+	EXPECT_EQ("A  A", TrimFront("A  A"));
+	EXPECT_EQ("A \tA", TrimFront("A \tA"));
+	EXPECT_EQ("A\t A", TrimFront("A\t A"));
+	EXPECT_EQ("A\t\tA", TrimFront("A\t\tA"));
+}
+
+TEST(StringConversion, TrimBack)
+{
+	// Empty string
+	EXPECT_EQ("", TrimBack(""));
+
+	// Only whitespace
+	EXPECT_EQ("", TrimBack(" "));
+	EXPECT_EQ("", TrimBack("\t"));
+	EXPECT_EQ("", TrimBack("\t "));
+	EXPECT_EQ("", TrimBack(" \t"));
+	EXPECT_EQ("", TrimBack("\t\t"));
+
+	// Only no whitespace
+	EXPECT_EQ("A", TrimBack("A"));
+	EXPECT_EQ("AA", TrimBack("AA"));
+	EXPECT_EQ("ABC", TrimBack("ABC"));
+
+	// Leading whitespace
+	EXPECT_EQ(" A", TrimBack(" A"));
+	EXPECT_EQ("\tA", TrimBack("\tA"));
+
+	// Trailing whitespace
+	EXPECT_EQ("A", TrimBack("A "));
+	EXPECT_EQ("A", TrimBack("A\t"));
+
+	// Leading and trailing whitespace
+	EXPECT_EQ(" A", TrimBack(" A "));
+	EXPECT_EQ("\tA", TrimBack("\tA\t"));
+	EXPECT_EQ("  A", TrimBack("  A  "));
+	EXPECT_EQ(" \tA", TrimBack(" \tA\t "));
+	EXPECT_EQ("\t A", TrimBack("\t A \t"));
+	EXPECT_EQ("\t\tA", TrimBack("\t\tA\t\t"));
+
+	// Embedded whitespace
+	EXPECT_EQ("A A", TrimBack("A A"));
+	EXPECT_EQ("A\tA", TrimBack("A\tA"));
+	EXPECT_EQ("A  A", TrimBack("A  A"));
+	EXPECT_EQ("A \tA", TrimBack("A \tA"));
+	EXPECT_EQ("A\t A", TrimBack("A\t A"));
+	EXPECT_EQ("A\t\tA", TrimBack("A\t\tA"));
+}
