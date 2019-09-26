bcec97ff2718c04e36b902c28f9de82f82833714
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-09-26 06:16:35 -0600

    Update documentation in StringConversion.h

--- srcStatic/StringConversion.h
+++ srcStatic/StringConversion.h
@@ -1,5 +1,3 @@
-// Functions that convert into and out of c-style strings and std::strings
-
 #pragma once
 
 #include <windows.h>
@@ -8,14 +6,19 @@
 #include <cstddef>
 
 
-// Convert a LPCTSTR to std::string
-// At compile time LPCTSTR will convert to either LPCSTR or LPCWSTR based on the UNICODE setting
+// Wraps either a narrow or wide raw string into either a std::string or std::wstring
+// Decision is made at compile time, though can be deferred until template instantiation
+// This may also potentially be used if a type depends on a #define setting.
+// Win32 Example: The UNICODE setting controls TCHAR, which controls LPTSTR and LPCTSTR
+// Function overload selection then results in the wrapped type std::string or std::wstring
 std::string WrapRawString(LPCSTR str);
 std::wstring WrapRawString(LPCWSTR str);
 
-// Converts a LPWSTR to std::string
+// Converts a wide string to a multibyte narrow string
 std::string ConvertWideToNarrow(std::wstring_view inputWideString, UINT codepage = CP_ACP);
 
+// Copies a std::string to a raw character buffer
+// This is used to interface with C code
 // Returns 0 on success
 // Returns needed buffer size (including space for null terminator) if the destination buffer is too small
 std::size_t CopyStdStringIntoCharBuffer(const std::string& stringToCopy, char* buffer, std::size_t bufferSize);
@@ -27,12 +30,18 @@ std::string& ToLowerInPlace(std::string& x);
 std::string ToLower(std::string x);
 
 
+// Trim whitespace from both ends or either end of a string
 std::string Trim(const std::string& stringToTrim, const std::string& whitespace = " \t");
 std::string TrimFront(const std::string& stringToTrim, const std::string& whitespace = " \t");
 std::string TrimBack(const std::string& stringToTrim, const std::string& whitespace = " \t");
 
+// Split string on all instances of a delimiter charater and return result in a vector of strings
+// The delimiter character is not included in the resulting strings
 std::vector<std::string> Split(std::string stringToSplit, char delimiter);
 
+// Split string on all instances of a delimiter character, and trim resulting strings, returning results in a vector
+// The delimiter character is not included in the resulting strings
+// If the delimiter character is a whitespace character, it is first used for splitting purposes
 template <typename std::string(TrimFunction)(const std::string&, const std::string&) = Trim>
 std::vector<std::string> SplitAndTrim(std::string stringToSplit, char delimiter, const std::string& whitespace = " \t") {
 	auto items = Split(stringToSplit, delimiter);
