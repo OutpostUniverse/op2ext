96dfaf9bc53126d13e2ba900527e129756e07a22
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-09-28 09:01:29 -0600

    Move log message test support code to separate file
    These classes might be re-used by many tests.

--- test/Log.test.cpp
+++ test/Log.test.cpp
@@ -1,5 +1,5 @@
 #include "Log.h"
-#include "Logger.h"
+#include "LogMessageTest.h"
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -8,27 +8,6 @@ TEST(Log, UnsetLoggerIsSafe) {
 	EXPECT_NO_THROW(SetLogger(nullptr));
 	EXPECT_NO_THROW(Log("This goes nowhere"));
 }
-
-
-class LoggerMock : public Logger {
-public:
-	MOCK_METHOD2(Log, void(const std::string& message, const std::string& moduleName));
-};
-
-
-class LogMessageTest : public ::testing::Test {
-protected:
-	void SetUp() override {
-		// Install test logger
-		EXPECT_NO_THROW(SetLogger(&logger));
-	}
-	void TearDown() override {
-		// Remove test logger
-		EXPECT_NO_THROW(SetLogger(nullptr));
-	}
-
-	LoggerMock logger;
-};
 
 
 TEST_F(LogMessageTest, ActiveLoggerReceivesMessages) {
