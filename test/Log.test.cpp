017d9f02b0a10ef04694b4ac03d888e64a824479
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-09-28 08:55:06 -0600

    Extract fixture for log message tests

--- test/Log.test.cpp
+++ test/Log.test.cpp
@@ -9,19 +9,31 @@ TEST(Log, UnsetLoggerIsSafe) {
 	EXPECT_NO_THROW(Log("This goes nowhere"));
 }
 
+
 class MockLogger : public Logger {
 public:
 	MOCK_METHOD2(Log, void(const std::string& message, const std::string& moduleName));
 };
 
-TEST(Log, ActiveLoggerReceivesMessages) {
-	const auto message = std::string("Logger should receive this");
+
+class LogMessageTest : public ::testing::Test {
+protected:
+	void SetUp() override {
+		// Install test logger
+		EXPECT_NO_THROW(SetLogger(&logger));
+	}
+	void TearDown() override {
+		// Remove test logger
+		EXPECT_NO_THROW(SetLogger(nullptr));
+	}
 
 	MockLogger logger;
-	EXPECT_CALL(logger, Log(message, "op2ext.dll"));
+};
 
-	EXPECT_NO_THROW(SetLogger(&logger));
+
+TEST_F(LogMessageTest, ActiveLoggerReceivesMessages) {
+	const auto message = std::string("Logger should receive this");
+	EXPECT_CALL(logger, Log(message, "op2ext.dll"));
 	EXPECT_NO_THROW(Log(message));
-	EXPECT_NO_THROW(SetLogger(nullptr));
 }
 
