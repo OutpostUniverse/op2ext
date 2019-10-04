26516b49ee1b657c72aa71aa156ec7561fa6af67
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-10-04 15:43:47 -0600

    Add test for `GetConsoleModDir_s`

--- testDll/op2ext.test.cpp
+++ testDll/op2ext.test.cpp
@@ -67,3 +67,27 @@ TEST(op2ext, GetGameDirMethodsGiveSameResult) {
 	// Result of both methods sould match, assuming sufficient buffer space
 	EXPECT_STREQ(gameDirectory, gameDirectorySafe);
 }
+
+TEST(op2ext, GetConsoleModDir_s) {
+	constexpr char Nonce = 255;
+	char consoleModDir[MAX_PATH] = {Nonce};
+
+	// No crash on nullptr buffer
+	EXPECT_NE(0u, GetConsoleModDir_s(nullptr, 0));
+	EXPECT_NE(0u, GetConsoleModDir_s(nullptr, MAX_PATH));
+
+	// Returns needed buffer size when supplied buffer is too small
+	// Buffer is not modified beyond specified length
+	EXPECT_NE(0u, GetConsoleModDir_s(consoleModDir, 0));
+	EXPECT_EQ(Nonce, consoleModDir[0]);
+
+	// On success return value is 0, and buffer contains path of specified format
+	EXPECT_EQ(0u, GetConsoleModDir_s(consoleModDir, MAX_PATH));
+	std::string_view consoleModDirView{consoleModDir};
+	// Path plus null terminator should fit within buffer size
+	ASSERT_TRUE(consoleModDirView.length() < MAX_PATH);
+	// Path is null terminated (this is outside the string_view window)
+	EXPECT_EQ(0, consoleModDir[consoleModDirView.length()]);
+	// Path ends with a trailing slash
+	EXPECT_EQ('\\', consoleModDirView.back());
+}
