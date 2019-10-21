1704df1df05ac5c2ab39eb13f7eace956601919a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-10-21 02:36:39 -0600

    Return relative path for `ConsoleModule::Directory`

--- srcStatic/GameModules/ConsoleModule.cpp
+++ srcStatic/GameModules/ConsoleModule.cpp
@@ -8,14 +8,15 @@
 
 ConsoleModule::ConsoleModule(const std::string& moduleName) : DllModule(moduleName)
 {
-	moduleDirectory = (fs::path(GetExeDirectory()) / moduleName).string() + "\\";
+	moduleDirectory = moduleName + "\\";
+	auto absoluteModuleDirectory = fs::path(GetExeDirectory()) / moduleDirectory;
 
-	if (!IsDirectory(moduleDirectory)) {
+	if (!IsDirectory(absoluteModuleDirectory.string())) {
 		throw std::runtime_error("Unable to access the provided module directory: " + 
 			moduleDirectory + " : " + GetLastErrorString());
 	}
 
-	const auto dllPath = fs::path(moduleDirectory).append("op2mod.dll").string();
+	const auto dllPath = (absoluteModuleDirectory / "op2mod.dll").string();
 	if (!Exists(dllPath)) {
 		return; // Some console modules do not contain dlls
 	}
