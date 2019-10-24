32110882508fef7ab28195b57b7610d3759e9dc4
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-10-24 07:38:21 -0600

    Make declaration order match implementation order

--- srcStatic/FileSystemHelper.h
+++ srcStatic/FileSystemHelper.h
@@ -7,7 +7,8 @@
 std::string GetExeDirectory();
 std::string GetOutpost2IniPath();
 std::string GetOutpost2IniSetting(const std::string& sectionName, const std::string& key);
-std::vector<std::string> FindFilesWithExtension(const std::string& basePath, const std::string& relativeSearchPath, std::string extension);
 
 bool IsDirectory(const std::string& path);
 bool Exists(const std::string& path);
+
+std::vector<std::string> FindFilesWithExtension(const std::string& basePath, const std::string& relativeSearchPath, std::string extension);
