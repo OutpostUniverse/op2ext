2e18a9f6ec8484897f57fc4918e0c74ca15b6750
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-10-26 21:59:47 -0600

    Remove `PostError` macro

--- srcStatic/Log.h
+++ srcStatic/Log.h
@@ -20,9 +20,3 @@ void LogError(const std::string& message);
 // Helpers to format log messages in standard ways
 std::string FormatLogMessage(const std::string& message, const std::string& moduleName = "op2ext.dll");
 std::string FormatLogMessage(const std::string& message, const std::string& sourcePathFilename, long lineInSourceCode);
-
-
-// Allow defaulting of the __FILE__ and __LINE__ macros at the point of call
-// Macro name must be different from original function name
-// Using the same name will cause an error when the compiler sees the underlying function definition
-#define PostError(message) LogError(FormatLogMessage(message, __FILE__, __LINE__))
