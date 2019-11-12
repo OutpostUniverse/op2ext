a1f43aa58384badb871face62bc365fb54a7dd62
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-11-11 18:51:01 -0700

    Re-order header methods to match implementation

--- srcStatic/OP2Memory.h
+++ srcStatic/OP2Memory.h
@@ -10,8 +10,8 @@
 bool EnableOp2MemoryPatching();
 
 bool Op2UnprotectMemory(std::uintptr_t destBaseAddress, std::size_t size);
-bool Op2MemCopy(std::uintptr_t destBaseAddress, std::size_t size, const void* sourceAddress);
 bool Op2MemSet(std::uintptr_t destBaseAddress, std::size_t size, unsigned char value);
+bool Op2MemCopy(std::uintptr_t destBaseAddress, std::size_t size, const void* sourceAddress);
 bool Op2WriteAddress(std::uintptr_t destBaseAddress, const void* newAddress);
 bool Op2RelinkCall(std::uintptr_t callInstructionAddress, const void* newFunctionAddress);
 
