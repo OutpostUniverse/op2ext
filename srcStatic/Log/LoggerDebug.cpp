12823d414493d7c446e0a06116e8ff18a6499d69
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-03-08 08:37:30 -0400

    Insert newline character automatically when calling LogDebug

--- srcStatic/Log/LoggerDebug.cpp
+++ srcStatic/Log/LoggerDebug.cpp
@@ -2,8 +2,15 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+#ifdef _WIN32
+const std::string newline("\r\n");
+#elif defined macintosh // OS 9
+const std::string newline("\r");
+#else
+const std::string newline("\n") // Mac OS X uses \n
+#endif
 
 void LoggerDebug::Log(const std::string& message)
 {
-	OutputDebugString(message.c_str());
+	OutputDebugString(std::string(message + newline).c_str());
 }
