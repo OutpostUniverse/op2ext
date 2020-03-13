5312606565b6c58013f5da7052603a359ae912e2
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-03-13 18:42:45 -0400

    Use hardcoded line ending instead of constant

--- srcStatic/Log/LoggerDebug.cpp
+++ srcStatic/Log/LoggerDebug.cpp
@@ -2,9 +2,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-const std::string newline("\n");
-
 void LoggerDebug::Log(const std::string& message)
 {
-	OutputDebugString((message + newline).c_str());
+	OutputDebugString((message + "\n").c_str());
 }
