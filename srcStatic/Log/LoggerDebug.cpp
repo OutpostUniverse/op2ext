80cd63359a2113ea9aba00c30a75d605ffe287c2
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-03-12 22:09:41 -0400

    Remove non-Windows newline strings

--- srcStatic/Log/LoggerDebug.cpp
+++ srcStatic/Log/LoggerDebug.cpp
@@ -2,15 +2,9 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-#ifdef _WIN32
 const std::string newline("\r\n");
-#elif defined macintosh // OS 9
-const std::string newline("\r");
-#else
-const std::string newline("\n") // Mac OS X uses \n
-#endif
 
 void LoggerDebug::Log(const std::string& message)
 {
-	OutputDebugString(std::string(message + newline).c_str());
+	OutputDebugString((message + newline).c_str());
 }
